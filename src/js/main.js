a0351a5eb2a546ff903df53aa483a4eaaba670c6
cegn <treinamento@mp2116.senado.gov.br>
2017-10-24 18:07:17 -0200

    segundo dia de aula

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
 var $ = require('jquery')
 var feature = require('./feature')
+
+
 console.log($.fn.jquery)
 console.log(feature.add(1, 2))
 console.log(feature.pow(9))
 console.log(feature.valor_padrao)
+
+$('body').on('click',function () {
+    $('body').css({
+        background: 'purple'
+    })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,37 @@
+var path = require('path')
+var Extractor = require('extract-text-webpack-plugin')
+
 module.exports = {
-    entry: './src/js/main.js',
-    output: {
-        filename: './src/js/saida.js'
-    }
+  entry: {
+    main: './src/js/main.js',
+    styles: './src/css/styles.less'
+  },
+  externals: {
+    jquery: 'jQuery'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve('./dist'),
+    publicPath: './'
+  },
+  module: {
+    rules: [{
+      test: /\.pug$/i,
+      loader: 'pug-loader'
+    }, {
+      test: /\.less$/i,
+      loader: Extractor.extract({
+        use: ['css-loader', 'less-loader']
+      })
+    }, {
+      test: /\.(svg|woff|ttf|eot|woff2)$/i,
+      loaders: ['file-loader?name=fonts/[name].[hash:base64:5].[ext]']
+    }]
+  },
+  
+  plugins: [
+    new Extractor({
+      filename: '[name].css', allChunks: true
+    })
+  ]
 }
